af3791e5bbaee9673537aefb1102d04f94cbd30e
Tinne Dey <118190717+tinne2@users.noreply.github.com>
2025-02-05 21:21:35 +0200

    Update mergesort.py

--- basics/mergesort.py
+++ basics/mergesort.py
@@ -5,24 +5,20 @@ def mergesort(array):
     m = len(array) // 2
     left = mergesort(array[:m])
     right = mergesort(array[m:])
-
     return merge(left, right)
 
 
 def merge(left, right):
     merged = []
-
     while len(left) > 0 and len(right) > 0:
         if left[0] <= right[0]:
             merged.append(left.pop(0))
         else:
             merged.append(right.pop(0))
-
     if len(left) > 0:
         merged += left
     else:
         merged += right
-
     return merged
 
 
@@ -31,7 +27,7 @@ if __name__ == "__main__":
 
     # Split input string, remove whitespace, and convert to integers
     input_list = [x.strip() for x in input_str.split(",")]
-    
+
     try:
         # Convert to integers
         value_list = [int(x) for x in input_list]
